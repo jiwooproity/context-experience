68563afc7f118cd1fe7ff00ba435638f796e4efa
jiwooproity <jiwooproity@naver.com>
2023-04-23 23:23:51 +0900

    practice : react-redux and use typescript -> make feature counter

---
+++ src/components/Counter.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+type CounterProps = {
+  count: number;
+  onIncrease: () => void;
+  onDecrease: () => void;
+  onIncreaseBy: (diff: number) => void;
+};
+
+const Counter = ({ count, onIncrease, onDecrease, onIncreaseBy }: CounterProps) => {
+  const [limit, setLimit] = useState<number>(5);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = +e.target.value;
+    setLimit(value);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <input type="number" value={limit} onChange={onChange}></input>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+      <button onClick={() => onIncreaseBy(limit)}>{`+${limit}`}</button>
+    </div>
+  );
+};
+
+export default Counter;
