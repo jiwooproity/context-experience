7f88264283a06eed6adc05deeb1fbd152094ea28
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2023-04-26 11:35:39 +0900

    practice : react-redux and use typescript -> make feature redux-todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import "./styles/app.css";
 import CounterContainer from "./components/CounterContainer";
+import ToDoListContainer from "./components/ToDoListContainer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <CounterContainer />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/count" element={<CounterContainer />}/>
+          <Route path="/todolist" element={<ToDoListContainer />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import counter from "./counter";
+import todolist from "./todolist";
 
 const rootReducer = combineReducers({
   counter,
+  todolist,
 });
 
 export default rootReducer;
